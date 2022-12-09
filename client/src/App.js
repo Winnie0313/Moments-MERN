617956535aecd2e511770bd31865d2764b9e0c77
Winnie0313 <liwanyi0313@gmail.com>
2022-12-09 15:49:39 -0800

    crete elements on the homepage

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,22 @@ const App = () => {
         <Typography variant="h2" align="center">Moments</Typography>
         <img src={moments} alt="moments" height="60" />
       </AppBar> 
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Post />
+
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+              
+              
+            </Grid>
+
+          </Grid>
+        </Container>
+      </Grow>
 
     </Container>
     
