0ff9e42397ceb25ebc56d47ea338cc1f0f358e53
Winnie0313 <liwanyi0313@gmail.com>
2022-12-08 16:50:01 -0800

    remove mongoose.set

--- server/index.js
+++ server/index.js
@@ -14,8 +14,7 @@ const CONNECTION_URL = 'mongodb+srv://winnie:wMh3OGIZDUHQ0zc0@cluster0.vp6cc2s.m
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-  .catch((error) => console.log(error.message));
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
 
-mongoose.set('useFindAndModify', false);
 
