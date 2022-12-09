87595f83200acdbdbb54ea932355abd2463b03db
Winnie0313 <liwanyi0313@gmail.com>
2022-12-08 16:36:28 -0800

    connect MongoDB

--- server/index.js
+++ server/index.js
@@ -9,3 +9,5 @@ app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
 
+// set up MongoDB
+const CONNECTION_URL = 'mongodb+srv://winnie:wMh3OGIZDUHQ0zc0@cluster0.vp6cc2s.mongodb.net/?retryWrites=true&w=majority'
