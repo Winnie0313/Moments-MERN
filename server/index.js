3be30c3de3a7b04cb9f0d055d76fd84e518b5468
Winnie0313 <liwanyi0313@gmail.com>
2022-12-08 16:43:59 -0800

    connect mongodb using mongoose

--- server/index.js
+++ server/index.js
@@ -11,3 +11,11 @@ app.use(cors());
 
 // set up MongoDB
 const CONNECTION_URL = 'mongodb+srv://winnie:wMh3OGIZDUHQ0zc0@cluster0.vp6cc2s.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+  .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
+
