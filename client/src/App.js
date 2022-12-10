eff18ed5fa0aae82ca9a526809ca417bbde1f5f4
Winnie0313 <liwanyi0313@gmail.com>
2022-12-09 16:15:00 -0800

    add styles

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,13 @@ import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 import moments from './images/moments.png';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
+import useStyles from './styles';
+import { mergeClasses } from "@material-ui/styles";
 
 const App = () => {
   return (
     <Container maxWidth="lg">
-      <AppBar position="static" color="interit">
+      <AppBar className={mergeClasses.appBar} position="static" color="interit">
         <Typography variant="h2" align="center">Moments</Typography>
         <img src={moments} alt="moments" height="60" />
       </AppBar> 
