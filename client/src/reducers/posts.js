2bcd25420a67fefa361843f2af563acf09d849f0
Winnie0313 <liwanyi0313@gmail.com>
2022-12-10 17:51:54 -0800

    create form

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,4 +1,4 @@
-// reducer a function that takes in the initial state and an action, and which returns that specific part of the global store
+// reducer - a function that takes in the initial state and an action, and which returns that specific part of the global store
 export default (posts = [], action) => {
   switch (action.type) {
     case 'FETCH_ALL':
