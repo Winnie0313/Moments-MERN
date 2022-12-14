4e26ff9a3d28b7bbfb8de41b9db0e20ea4bd8ef2
Winnie0313 <liwanyi0313@gmail.com>
2022-12-13 22:08:50 -0800

    loop over the posts array to render each post

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -11,11 +11,14 @@ const Posts = () => {
 
   console.log(posts);
   return (
-    !posts.length ? <CircularProgess /> : (
+    // conditionally render component
+    !posts.length ? <CircularProgress /> : (
       <Grid className={classes.container} container alignItems="stretch" spacing={3} >
-        {
-          
-        }
+          {posts.map((post) => (
+            <Grid key={post._id} item xs={12} sm={6}>
+              <Post post={post}/>
+            </Grid>
+          ))}  
       </Grid>
     )
     
